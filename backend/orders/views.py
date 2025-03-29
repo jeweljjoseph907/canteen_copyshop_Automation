f4212b8815dae585cb1225de7f39e070122b5133
Mhmd-Aslam <aslamaass108@gmail.com>
2025-03-29 13:27:27 +0530

    frontend

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def test_api(request):
+    return JsonResponse({"message": "Django backend is working!"})
